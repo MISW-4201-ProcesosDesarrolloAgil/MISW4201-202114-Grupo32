7bfe2a843fbb3751d640725185b046dd4c5e8711
Félix Acero <acerofel@uniandes.edu.co>
2021-09-22 18:46:59 -0500

    IFOOTER: Inclusión del footer y el about

---
+++ front-end/tutorial-canciones/src/footer/footer.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor(
+    private routerPath: Router,
+    private router: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+
+  goTo(menu: string) {
+    const userId = parseInt(this.router.snapshot.params.userId)
+    const token = this.router.snapshot.params.userToken
+    if (menu === "about") {
+      this.routerPath.navigate([`/about/${userId}/${token}`])
+    }
+  }
+  status: boolean = false;
+  clickEvent() {
+    this.status = !this.status;
+  }
+}
